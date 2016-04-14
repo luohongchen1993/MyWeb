c140a8d4f75224bb0b4f4e5f42d4cba3504d4e1d
luohongchen1993 <luohongchen1993@gmail.com>
2016-04-14 19:38:19 -0400

    added textmining part

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask.ext.moment import Moment
 from datetime import datetime
+import re
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -19,6 +20,10 @@ moment = Moment(app)
 class NameForm(Form):
     name = StringField('What is your favorite movie?', validators=[Required()])
     submit = SubmitField('Submit')
+	
+class TextForm(Form):
+	text = StringField('What is the text you wanna analyze?', validators = [Required()])
+	submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -39,6 +44,40 @@ def index():
         name = form.name.data
         form.name.data = ''
     return render_template('index.html', form=form, name=name,current_time=datetime.utcnow())
+
+def text_mining_engine(textdata):
+	positive = ['love', 'like', 'enjoy', 'good', 'happy', 'great', 'wonderful', 'terrific', 'cool', 'best']
+	negative = ['bad', 'worst', 'sad', 'unhappy', 'unpleasant', 'hate', 'angry', 'awful', 'poor','annoying']
+	textdata = ' """'+textdata+ '""" '
+	textdata = re.sub('[^a-zA-Z]',' ',textdata)
+	textdata = textdata.lower()
+	textdata_array = textdata.split(' ')
+
+	pos_count = 0
+	neg_count = 0
+
+	for i in textdata_array:
+		if i in positive:
+			pos_count+=1
+		
+		elif i in negative:
+			neg_count+=1
+			
+	return pos_count,neg_count
+	
+@app.route('/textmining', methods=['GET', 'POST'])
+def textmining():
+	text = None
+	pos_count = None
+	neg_count = None
+	form = TextForm()
+	if form.validate_on_submit():
+		text = form.text.data
+		form.text.data = ''
+		textdata = text
+		pos_count,neg_count = text_mining_engine(textdata)
+		
+	return render_template('textmining.html', form=form,text=text, pos_count=pos_count,neg_count=neg_count,current_time=datetime.utcnow())
 
 
 if __name__ == '__main__':
