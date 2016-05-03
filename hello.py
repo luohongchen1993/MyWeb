217d41d642b26caae05f57ae3d04bc2117d9551e
luohongchen1993 <luohongchen1993@gmail.com>
2016-05-03 09:23:26 -0400

    Async Email

--- hello.py
+++ hello.py
@@ -15,6 +15,7 @@ from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.mail import Mail
 from flask.ext.mail import Message
 import pdb
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -63,14 +64,19 @@ class User(db.Model):
 		
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-
+def send_async_email(app, msg):
+	with app.app_context():
+		mail.send(msg)
 # send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
 def send_email(to, subject, template, **kwargs):
 	msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
 		sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
 	msg.body = render_template(template + '.txt', **kwargs)
 	msg.html = render_template(template + '.html', **kwargs)
-	mail.send(msg)
+	# mail.send(msg)
+	thr = Thread(target=send_async_email, args=[app, msg])
+	thr.start()
+	return thr
 
 
 
